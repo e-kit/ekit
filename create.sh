2513a5ce61311b93e933da18e03c871656446847
yangqianjun <yangqianjun@kuaishou.com>
2020-10-16 21:08:34 +0800

    chore: version

--- create.sh
+++ create.sh
@@ -4,7 +4,7 @@ cp packages/typescript-type-tester/jest.config.js packages/$1
 cp packages/typescript-type-tester/tsconfig.json packages/$1
 cp packages/typescript-type-tester/tsconfig.prod.json packages/$1
 cp packages/typescript-type-tester/tsconfig.test.json packages/$1
-node -e "const json=require('./packages/$1/package.json');json.scripts.test='jest --all';json.scripts.build = 'tsc --project tsconfig.prod.json';json.version='0.0.0';json.main='lib/index.js';json.description = '${1}'.split('-').join(' ');json.license='MIT';require('fs').writeFileSync('./packages/$1/package.json', JSON.stringify(json, null, '  '), { encoding: 'utf8' });"
+node -e "const json=require('./packages/$1/package.json');json.scripts.test='jest --all';json.scripts.build = 'tsc --project tsconfig.prod.json';json.version='0.0.1';json.main='lib/index.js';json.description = '${1}'.split('-').join(' ');json.license='MIT';require('fs').writeFileSync('./packages/$1/package.json', JSON.stringify(json, null, '  '), { encoding: 'utf8' });"
 rm -rf packages/$1/__tests__/$1.test.js
 rm -rf packages/$1/lib/$1.js
 mkdir -p packages/$1/src
