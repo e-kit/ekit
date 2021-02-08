2f1c2a69bf486df024ff0847f9c08b55fc0943e4
yangqianjun <yangqianjun@kuaishou.com>
2021-02-08 17:48:14 +0800

    refactor(types): 格式化

--- packages/types/__tests__/Doc.tsx
+++ packages/types/__tests__/Doc.tsx
@@ -1,9 +1,5 @@
 /**
  * @file: for mock docs
- * @author: yangqianjun
- * @Date: 2019-12-19 11:35:24
- * @LastEditors: yangqianjun
- * @LastEditTime: 2019-12-19 13:11:44
  */
 import React from 'react';
 import { TkitUtils } from '../types/utils';
