8236807036e230a99f758fd15700b58630393be9
yangqianjun <yangqianjun@kuaishou.com>
2021-02-08 14:35:56 +0800

    test(actions): 修正单测

--- packages/actions/__tests__/actions.spec.ts
+++ packages/actions/__tests__/actions.spec.ts
@@ -3,7 +3,7 @@ import { parser } from '@ekit/typescript-type-tester';
 const files = 'actionsOK.ts actionsFail.ts'.split(' ');
 
 describe('actions work ok', () => {
-  const errors = parser(files, 'actionsSamples');
+  const errors = parser(files, '__tests__/actionsSamples');
   files.forEach((file) =>
     it(`check sample/${file} work ok`, () => {
       const current = errors[file];
--- packages/actions/src/actions.ts
+++ packages/actions/src/actions.ts
@@ -1,6 +1,7 @@
 import { action } from 'typesafe-actions';
 
 export const createAction = action;
+export { bindActionCreators } from 'redux';
 export * from 'redux-actions';
 export { Action } from 'redux-actions';
 export interface ActionWithPayload<P> {
