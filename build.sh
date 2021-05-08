148a2bfc981803c1e994f82ad54045be97f4abf5
tianzhi03 <tianzhi03@kuaishou.com>
2021-05-08 18:59:45 +0800

    build: 更新 build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -e
-pks="utils jest-config event-center event ajax actions tkit model-factory redux-model use-model model async editor pagination use-fetch eslint-config-tkit scripts service"
+pks="utils jest-config event-center event ajax actions tkit model-factory redux-model use-model model async editor pagination use-fetch eslint-config-tkit scripts service typescript-type-tester"
 
 for p in $pks; do
   if [ -d packages/$p ]; then
